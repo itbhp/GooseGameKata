6a9e641faaf29a38a72202f14bb1cb77a1d56646
Paolo Sciarra <itbhps@gmail.com>
2023-04-21 18:44:45 +0200

    now we can quit the game

--- src/main/java/it/twinsbrain/dojos/GooseGame.java
+++ src/main/java/it/twinsbrain/dojos/GooseGame.java
@@ -1,4 +1,18 @@
 package it.twinsbrain.dojos;
 
+import java.io.InputStream;
+import java.io.OutputStream;
+
 public class GooseGame {
+    private final InputStream input;
+    private final OutputStream output;
+
+    public GooseGame(InputStream input, OutputStream output) {
+        this.input = input;
+        this.output = output;
+    }
+
+    public void play() throws Exception {
+        output.write("See you!".getBytes());
+    }
 }
--- src/test/java/it/twinsbrain/dojos/GooseGameTest.java
+++ src/test/java/it/twinsbrain/dojos/GooseGameTest.java
@@ -1,5 +1,24 @@
 package it.twinsbrain.dojos;
 
+import org.junit.jupiter.api.Test;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+
 class GooseGameTest {
 
+    @Test
+    void should_allow_to_quit_game() throws Exception {
+        var commands = Stream.of("quit").collect(Collectors.joining("\n"));
+        var input = new ByteArrayInputStream(commands.getBytes());
+        var output = new ByteArrayOutputStream();
+        new GooseGame(input, output).play();
+
+        assertThat(output.toString(), equalTo("See you!"));
+    }
 }
